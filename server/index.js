f3b0ba09c5351fa642d152e5836c42c613c7a2c1
Tagir <tttggg2004@mail.ru>
2024-11-28 01:33:22 +0300

    Changed index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const start =  async () => {
     try{
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(PORT, () => console.log(`HAHAHAHA port ${PORT}`))
+        app.listen(PORT, () => console.log(`Listening port ${PORT}`))
     } catch (e) {
         console.log(e)
     }
