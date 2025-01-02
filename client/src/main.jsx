2aafd2f7390c3901a19956b287aac5bbabeb8365
Tagir <tttggg2004@mail.ru>
2025-01-02 05:02:35 +0300

    Перенёс старую структуру React (create-react-app) на Vite

---
+++ client/src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import React from 'react';
+import { Provider } from 'react-redux'
+import index from './store/index.js';
+import { BrowserRouter } from 'react-router-dom';
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter  basename='/todo-app'>
+    <Provider store={index}>
+    <App />
+    </Provider>
+  </BrowserRouter>
+)
