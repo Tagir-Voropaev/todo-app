594a38e5411ae847e2e1fa294bc0a6b97911da42
Tagir <tttggg2004@mail.ru>
2025-01-06 16:57:39 +0300

    Начал верстку страниц

--- client/src/components/pages/Tasks/Tasks.jsx
+++ client/src/components/pages/Tasks/Tasks.jsx
@@ -1,48 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import "../../../static/css/components/tasks/Tasks.css"
-import "../../../static/css/components/tasks/SearchTask.css"
-import "../../../static/css/components/tasks/TaskList.css"
-import AddTask from './AddTask'
-import SearchTask from './SearchTask'
-import TaskList from './TaskList'
+import React from 'react'
+import s from '../../../static/css/components/tasks/tasks.module.scss'
 const Tasks = () => {
 
-
-    const [hideAdd, setHideAdd] = useState(false)
-
-
-    useEffect(() => {
-        setHideAdd(false)
-    }, [])
-
-    //Стейты
-    //Показать/скрыть форму добавления задач
-    const toggleAdd = () => {
-        if (hideAdd) {
-            setHideAdd(false)
-        } else {
-            setHideAdd(true)
-        }
-    }
     return (
-        <div className="wrapper">
-            <div className="window-block">
-                <h1 className='window-title'>Задачи</h1>
-                <div className="task-top">
-                    <SearchTask />
-                    <button
-                        className='task-button-open'
-                        onClick={toggleAdd}
-                    >
-                        {hideAdd ? 'Закрыть' : 'Добавить'}
-                    </button>
-                </div>
-                <div className='task-hide'>
-                    {hideAdd && <AddTask />}
-                </div>
-                <div className="tasks">
-                    <TaskList />
-                </div>
+        <div className={s.tasks}>
+            <div className={s.categoryHeader}>
+                <p></p>
             </div>
         </div>
     )
